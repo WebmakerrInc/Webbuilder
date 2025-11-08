fc12b066d6ad0cbdfef5e9884a3868a1b009d6be
taskpul <romtietz@gmail.com>
2025-11-08 18:49:48 +0100

    Fix sanitize callback references for GrapesJS meta

--- grapesjs-page-builder/includes/class-grapesjs-loader.php
+++ grapesjs-page-builder/includes/class-grapesjs-loader.php
@@ -61,7 +61,7 @@ class Loader {
                 'type'              => 'string',
                 'single'            => true,
                 'show_in_rest'      => true,
-                'sanitize_callback' => '\\wp_kses_post',
+                'sanitize_callback' => '\wp_kses_post',
                 'auth_callback'     => static function ( $allowed, $meta_key, $post_id ) {
                     return \current_user_can( 'edit_post', $post_id );
                 },
@@ -75,7 +75,7 @@ class Loader {
                 'type'              => 'string',
                 'single'            => true,
                 'show_in_rest'      => true,
-                'sanitize_callback' => '\\sanitize_textarea_field',
+                'sanitize_callback' => '\sanitize_textarea_field',
                 'auth_callback'     => static function ( $allowed, $meta_key, $post_id ) {
                     return \current_user_can( 'edit_post', $post_id );
                 },
